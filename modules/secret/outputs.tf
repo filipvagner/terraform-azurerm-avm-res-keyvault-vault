9869d0c6eb3b3e525673b414e8f7155d659402e0
Filip Vagner <filip.vagner@orbit.cz>
2025-08-07 21:56:15 +0200

    Added condition for output

--- modules/secret/outputs.tf
+++ modules/secret/outputs.tf
@@ -1,19 +1,19 @@
 output "id" {
   description = "The Key Vault Secret ID"
-  value       = azurerm_key_vault_secret.this[0].id
+  value = var.value == "" ? azurerm_key_vault_secret.unmanaged[0].id : azurerm_key_vault_secret.this[0].id
 }
 
 output "resource_id" {
   description = "The Azure resource id of the secret."
-  value       = azurerm_key_vault_secret.this[0].resource_id
+  value       = var.value == "" ? azurerm_key_vault_secret.unmanaged[0].resource_id : azurerm_key_vault_secret.this[0].resource_id
 }
 
 output "resource_versionless_id" {
   description = "The versionless Azure resource id of the secret."
-  value       = azurerm_key_vault_secret.this[0].resource_versionless_id
+  value       = var.value == "" ? azurerm_key_vault_secret.unmanaged[0].resource_versionless_id : azurerm_key_vault_secret.this[0].resource_versionless_id
 }
 
 output "versionless_id" {
   description = "The Base ID of the Key Vault Secret"
-  value       = azurerm_key_vault_secret.this[0].versionless_id
+  value       = var.value == "" ? azurerm_key_vault_secret.unmanaged[0].versionless_id : azurerm_key_vault_secret.this[0].versionless_id
 }
